136d739580e6392a8cc90abd22cc7d4078fc6b9a
polar-cloud <grahem@gmail.com>
2019-11-09 08:52:48 -0800

    fixed contact FirstName

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Site.Models;
+
 
 namespace Site.Controllers
 {
@@ -28,7 +28,7 @@ namespace Site.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
 
     }
--- ServiceModels/Contact.cs
+++ ServiceModels/Contact.cs
@@ -9,7 +9,7 @@ namespace Site.ServiceModels
     public class Contact
     {
         public String Id { get; set; }
-        public String FirsName { get; set; }
+        public String FirstName { get; set; }
         public String LastName { get; set; }
         public String Email { get; set; }
         public String Phone { get; set; }
